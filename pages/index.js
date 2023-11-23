85268aed779511d73d79a3197dd6bf8b748b95bc
Phelipe Mascarenhas <35925107+phmasc@users.noreply.github.com>
2023-11-23 19:35:00 +0000

    adiciona o pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Sabemos que Deus age em todas as coisas para o bem daqueles que o amam, dos que foram chamados de acordo com o seu propósito.</h1>
+}
+
+export default Home;
