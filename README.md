5d61b61144257aaab818d0bb4e6aa13b826553bb
Phelipe Mascarenhas <35925107+phmasc@users.noreply.github.com>
2023-11-24 20:26:56 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do TabNews para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Sabemos que Deus age em todas as coisas para o bem daqueles que o amam, dos que foram chamados de acordo com o seu propósito.</h1>
+function Home() {
+  return (
+    <h1>
+      Sabemos que Deus age em todas as coisas para o bem daqueles que o amam,
+      dos que foram chamados de acordo com o seu propósito.
+    </h1>
+  );
 }
 
 export default Home;
