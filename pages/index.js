015e4e723dc3254d6bca995749d1951d9190f6d3
Phelipe Mascarenhas <35925107+phmasc@users.noreply.github.com>
2023-12-07 18:20:57 +0000

    Mudança de Texto

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home() {
   return (
     <h1>
-      Sabemos que Deus age em todas as coisas para o bem daqueles que o amam,
-      dos que foram chamados de acordo com o seu propósito.
+      <b>Heidy</b> te amo demais.
     </h1>
   );
 }
