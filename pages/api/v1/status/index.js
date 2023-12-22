418c51345127a6bac345430f77f240419470dcab
Phelipe Mascarenhas <ph.masc@gmail.com>
2023-12-22 07:59:52 -0300

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,9 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "Alunos do Curso.dev são pessoas acima da média" });
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query("Select 1 + 1 as sum;")
+  console.log(result.rows)
+  response.status(200).json({ chave: "Alunos do Curso.dev são pessoas acima da média" });
 }
 
 export default status;
